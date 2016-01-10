b23ce6e5b85c438881ed3b4c6e4803fdb64462dc
Gerald Stanje <Gerald.Stanje@gmail.com>
2016-01-10 03:19:42 +0100

    changed var names, formatted code

--- include/solver.h
+++ include/solver.h
@@ -9,40 +9,40 @@
 #include <memory>
 
 class Token {
-public:
-  virtual ~Token() {}
+  public:
+    virtual ~Token() {}
 };
 
 class Integer: public Token {
-public:
-  unsigned int value;
-  Integer(unsigned int val): value(val) {}
+  public:
+    unsigned int value;
+    Integer(unsigned int val): value(val) {}
 };
 
 class Variable: public Token {
-public:
-  std::string var_name;
-  Variable(const std::string name): var_name(name) {}
+  public:
+    std::string var_name;
+    Variable(const std::string name): var_name(name) {}
 };
 
 class Solver {
-private:
-  // the equation has the following format:
-  // var_name_lhs -> total_sum -> var1 -> var2
-  std::vector<std::shared_ptr<Token>> curr_equation;
-  // stores all equations
-  std::queue<std::vector<std::shared_ptr<Token>>> equation;
-  // all lhs variables which are known are stored in the hashtable as follows:
-  // variable_lhs, unsigned integer value
-  std::unordered_map<std::string, unsigned int> variable_ht;
+  private:
+    // the equation has the following format:
+    // var_name_lhs -> total_sum -> var1 -> var2
+    std::vector<std::shared_ptr<Token>> curr_equation;
+    // stores all equations
+    std::queue<std::vector<std::shared_ptr<Token>>> equations;
+    // all lhs variables which are known are stored in the hashtable as follows:
+    // variable_lhs, unsigned integer value
+    std::unordered_map<std::string, unsigned int> lhs_var_ht;
 
-public:
-  Solver();
-  void add_equation(std::string var_name_lhs);
-  void add_variable(std::string var_name);
-  void add_number(unsigned int value);
-  void solve();
-  void print_solution();
+  public:
+    Solver();
+    void add_equation(std::string var_name_lhs);
+    void add_variable(std::string var_name);
+    void add_number(unsigned int value);
+    void solve();
+    void print_solution();
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -9,17 +9,17 @@ extern FILE *yyin;
 extern Solver solver;
 
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    fprintf(stderr, "usage: %s filename\n", argv[0]);
-    exit(1);
-  }
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s filename\n", argv[0]);
+        exit(1);
+    }
 
-  yyin = fopen(argv[1], "r+");
-  yyparse();
-  fclose(yyin);
+    yyin = fopen(argv[1], "r+");
+    yyparse();
+    fclose(yyin);
 
-  solver.solve();
-  solver.print_solution();
+    solver.solve();
+    solver.print_solution();
 
-  return 0;
+    return 0;
 }
